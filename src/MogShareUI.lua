7f54f8bf6103fa89c1458cde07a65cd0c914ccc0
opussf <github@zz9-za.com>
2026-09-20 14:34:14 -0700

    Removing commented lines, and reference code.

--- src/MogShareUI.lua
+++ src/MogShareUI.lua
@@ -15,9 +15,6 @@ function MS.Set_mixin:OnRowClick(button)
 			if actor then
 				actor:Undress()
 			end
-			-- if DressUpFrameResetButton then
-				-- DressUpFrameResetButton:Click()
-			-- end
 			SetItemRef(linkType..":"..linkData, self.link, button)
 		end
 	end
@@ -135,97 +132,3 @@ function MS.UI_ShowList()
 		count = count + 1
 	end
 end
-
-
---[[
-
-INEED_SLUG, INEED   = ...
-
-function INEED.Sorted( t, f )
-	local a = {}
-	for n in pairs( t ) do table.insert( a, n ) end
-	table.sort( a, f ) -- @TODO: Look into giving a sort function here.
-	local i = 0
-	local iter = function()
-		i = i + 1
-		if a[i] == nil then return nil
-		else return a[i], t[a[i]]   --[[
-		end
-	end
-	return iter
-end
-function INEED.Fulfill_OnShow()
-	INEED_FulfillFrame:RegisterEvent("BAG_UPDATE")
-	INEED.Fulfill_BuildItemDisplay()
-	INEED.Fulfill_ShowItems()
-end
-function INEED.Fulfill_OnHide()
-	INEED_FulfillFrame:UnregisterEvent("BAG_UPDATE")
-end
-function INEED.Fulfill_ShowItems()
-	local count = 1
-	local sortedItems = {}
-	for k in pairs( INEED.fulfillList ) do table.insert(sortedItems, k) end
-	table.sort( sortedItems )
-
-	while count <= #INEED.Fulfill_ItemFrames do
-		itemID = sortedItems[count]
-		local itemFrame = INEED.Fulfill_ItemFrames[count]
-		local name, itemLink, icon = nil, nil, nil
-		if itemID then
-			name, itemLink, _, _, _, _, _, _, _, icon = GetItemInfo( itemID )
-		end
-
-		itemFrame.itemLink = itemLink
-		itemFrame.itemName = name
-		SetItemButtonTexture( itemFrame, icon )
-
-		count = count + 1
-	end
-end
-function INEED.Fulfill_BAG_UPDATE( self )
-	INEED.makeFulfillList()
-	INEED.Fulfill_ShowItems()
-end
-function INEED.Fulfill_BuildItemDisplay()
-	if not INEED.Fulfill_ItemFrames then
-		local width, height = INEED_FulfillFrame:GetSize()
-		local rowSize = math.floor( width / 32 )
-		local colSize = math.floor( (height - 50) / 32 )
-		local itemFrame
-
-		INEED.Fulfill_ItemFrames = {}
-
-		for itemFrameNum = 1, rowSize * colSize do -- rowSize * colSize do
-			itemFrame = CreateFrame( "Button", "INEED_FulfillFrameItem"..itemFrameNum, INEED_FulfillFrame, "INEEDItemTemplate" )
-			local col = ((itemFrameNum - 1) % rowSize) + 1
-			local row = math.floor( (itemFrameNum-1) / rowSize ) + 1
-
-			if row == 1 then
-				itemFrame:SetPoint( "TOP", INEED_FulfillFrame_Title, "BOTTOM" )
-			else
-				itemFrame:SetPoint( "TOP", INEED.Fulfill_ItemFrames[itemFrameNum-rowSize], "BOTTOM" )
-			end
-			if col == 1 then
-				itemFrame:SetPoint( "LEFT", INEED_FulfillFrame, "LEFT" )
-			else
-				itemFrame:SetPoint( "LEFT", INEED.Fulfill_ItemFrames[itemFrameNum-1], "RIGHT" )
-			end
-			INEED.Fulfill_ItemFrames[itemFrameNum] = itemFrame
-		end
-	end
-end
-function INEED.Fulfill_OnMouseDown( self, useParent )
-	local frameToRaise = useParent and self:GetParent() or self
-	INEED.fulfillOriginalStrata = INEED.fulfillOriginalStrata or frameToRaise:GetFrameStrata()
-	frameToRaise:SetFrameStrata("HIGH")
-	frameToRaise:Raise()
-end
-function INEED.Fulfill_OnLeave(self)
-	if INEED.fulfillOriginalStrata then
-		self:SetFrameStrata( INEED.fulfillOriginalStrata )
-	end
-end
-
-
-]]
